5fb15bf921b4cb3eecb7e40a1ade9d3a7f0467f7
charbell12 <chli0032@student.hv.se>
2026-02-24 14:43:17 +0100

    Remove "Hello World!"

--- RidersGitHubDemo/Program.cs
+++ RidersGitHubDemo/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         Console.WriteLine("Det här en kod ändring!");
         Console.WriteLine("New Line alert!");
         Console.WriteLine("I said: New Line Alert!");
